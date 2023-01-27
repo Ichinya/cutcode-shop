de6772da82e06b8427447e77bc8bf2dfc45598fb
Ичи <ichi-perm@yandex.ru>
2023-01-27 09:07:08 +0500

    добавление rate limit для сайта

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -47,6 +48,12 @@ class RouteServiceProvider extends ServiceProvider
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(600)
+                ->by($request->user()?->id ?: $request->ip())
+                ->response(fn(Request $request, array $headers) => response('Too many request', Response::HTTP_TOO_MANY_REQUESTS, $headers));
         });
     }
 }
